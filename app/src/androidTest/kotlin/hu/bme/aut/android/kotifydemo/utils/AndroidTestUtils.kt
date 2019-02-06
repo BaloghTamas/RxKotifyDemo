67f6a69bdf3c5ffb4089f77fbd81a171456b6393
Balogh Tamás <balogh.tamas@autsoft.hu>
2019-02-06 14:53:20 +0100

    Instrumentation test missing injector fixed, dependencies updated. Glide updated.

--- app/src/androidTest/kotlin/hu/bme/aut/android/kotifydemo/utils/AndroidTestUtils.kt
+++ app/src/androidTest/kotlin/hu/bme/aut/android/kotifydemo/utils/AndroidTestUtils.kt
@@ -2,6 +2,7 @@ package hu.bme.aut.android.kotifydemo.utils
 
 import android.support.test.InstrumentationRegistry
 import hu.bme.aut.android.kotifydemo.AndroidTestModule
+import hu.bme.aut.android.kotifydemo.DaggerAndroidTestComponent
 import hu.bme.aut.android.kotifydemo.DaggerKotifyDemoApplicationComponent
 import hu.bme.aut.android.kotifydemo.KotifyDemoApplication
 import hu.bme.aut.android.kotifydemo.ui.UIModule
